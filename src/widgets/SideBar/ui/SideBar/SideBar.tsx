ebd283a06b683267895690110bdeada3198bee9f
Sergey Romanov <sergeyromanovv94@gmail.com>
2023-12-07 18:33:35 +0300

    Added EsLint, set up the configuration, fix all files

--- src/widgets/SideBar/ui/SideBar/SideBar.tsx
+++ src/widgets/SideBar/ui/SideBar/SideBar.tsx
@@ -1,29 +1,29 @@
 import { FC, useState } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
-import cls from './SideBar.module.scss';
 import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
 import { LangSwitcher } from 'widgets/LangSwitcher/LangSwitcher';
+import cls from './SideBar.module.scss';
 
 interface SideBarProps {
   className?: string;
 }
 
 export const SideBar: FC<SideBarProps> = ({ className }) => {
-  const [collapsed, setCollapsed] = useState(false);
+    const [collapsed, setCollapsed] = useState(false);
 
-  const onToggle = () => {
-    setCollapsed((prev) => !prev);
-  }
-  
-  return (
-    <div 
-      className={classNames(cls.SideBar, {[cls.collapsed]: collapsed}, [className])}
-    >
-      <button onClick={onToggle}>TOOGLE</button>
-      <div className={cls.switchers}>
-        <ThemeSwitcher />
-        <LangSwitcher className={cls.lang} />
-      </div>
-    </div>
-  );
+    const onToggle = () => {
+        setCollapsed((prev) => !prev);
+    };
+
+    return (
+        <div
+            className={classNames(cls.SideBar, { [cls.collapsed]: collapsed }, [className])}
+        >
+            <button type="button" onClick={onToggle}>TOOGLE</button>
+            <div className={cls.switchers}>
+                <ThemeSwitcher />
+                <LangSwitcher className={cls.lang} />
+            </div>
+        </div>
+    );
 };
