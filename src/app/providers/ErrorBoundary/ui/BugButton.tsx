ed876f3cdc15cac78d6962ff632ff7f984683eec
Sergey Romanov <sergeyromanovv94@gmail.com>
2023-12-14 16:14:02 +0300

    Handling React errors, add ErrorBoundary

---
+++ src/app/providers/ErrorBoundary/ui/BugButton.tsx
@@ -0,0 +1,21 @@
+import { FC, useEffect, useState } from 'react';
+import { Button } from 'shared/ui/Button/Button';
+
+export const BugButton: FC = () => {
+    const [error, setError] = useState(false);
+
+    const onThrow = () => setError(true);
+
+    useEffect(() => {
+        if (error) throw new Error();
+    }, [error]);
+
+    return (
+        <Button
+            onClick={onThrow}
+        // eslint-disable-next-line i18next/no-literal-string
+        >
+            throw error
+        </Button>
+    );
+};
