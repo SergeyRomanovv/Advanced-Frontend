0c516b01193c4dd97e29527c4916d913136cb5b9
Sergey Romanov <sergeyromanovv94@gmail.com>
2024-02-15 01:54:38 +0300

    add modal, portal, change header

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -2,7 +2,7 @@ import { BugButton } from 'app/providers/ErrorBoundary';
 import { useTranslation } from 'react-i18next';
 
 const MainPage = () => {
-    const { t } = useTranslation('main');
+    const { t } = useTranslation();
 
     return (
         <div>
