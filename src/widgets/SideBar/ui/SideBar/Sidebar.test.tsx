ab57ff09502a9168f869fbaeb503951c07117dc8
Sergey Romanov <sergeyromanovv94@gmail.com>
2024-02-14 18:47:38 +0300

    fix test add new until componentRender for tests

--- src/widgets/SideBar/ui/SideBar/Sidebar.test.tsx
+++ src/widgets/SideBar/ui/SideBar/Sidebar.test.tsx
@@ -1,16 +1,17 @@
 import { fireEvent, screen } from '@testing-library/react';
 // eslint-disable-next-line max-len
 import { renderwithTranslation } from 'shared/lib/tests/renderwithTranslation/renderwithTranslation';
+import { componentRender } from 'shared/lib/tests/componentRender/componentRender';
 import { SideBar } from './SideBar';
 
 describe('Sidebar', () => {
     test('Test render', () => {
-        renderwithTranslation(<SideBar />);
+        componentRender(<SideBar />);
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     });
 
     test('Test toggle', () => {
-        renderwithTranslation(<SideBar />);
+        componentRender(<SideBar />);
         const toggleBtn = screen.getByTestId('sidebar-toggle');
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
         fireEvent.click(toggleBtn);
