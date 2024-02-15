64e9598cce7207d73c93d7d84275ef583fd89117
Sergey Romanov <sergeyromanovv94@gmail.com>
2024-02-15 18:57:54 +0300

    add redux store, add first entity

--- src/index.tsx
+++ src/index.tsx
@@ -5,15 +5,18 @@ import { ErrorBoundary } from 'app/providers/ErrorBoundary';
 import App from './app/App';
 import 'shared/config/i18n/i18n';
 import 'app/styles/index.scss';
+import { StoreProvider } from './app/providers/StoreProvider';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
-    <BrowserRouter>
-        <ErrorBoundary>
-            <ThemeProvider>
-                <App />
-            </ThemeProvider>
-        </ErrorBoundary>
-    </BrowserRouter>,
+    <StoreProvider>
+        <BrowserRouter>
+            <ErrorBoundary>
+                <ThemeProvider>
+                    <App />
+                </ThemeProvider>
+            </ErrorBoundary>
+        </BrowserRouter>
+    </StoreProvider>,
 );
