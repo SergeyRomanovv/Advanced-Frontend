5995fd3faadb3409e046931b228163f87d80147e
Sergey Romanov <sergeyromanovv94@gmail.com>
2024-05-09 16:36:35 +0300

    create entity article, async thunk, slices, skeleton loader

--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
@@ -1,6 +1,8 @@
 import { FC, memo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { classNames } from 'shared/lib/classNames/classNames';
+import { ArticleDetails } from 'entities/Article';
+import { useParams } from 'react-router-dom';
 import cls from './ArticleDetailsPage.module.scss';
 
 interface ArticleDetailsPageProps {
@@ -10,11 +12,20 @@ interface ArticleDetailsPageProps {
 const ArticleDetailsPage: FC<ArticleDetailsPageProps> = (props) => {
     const { className } = props;
     const { t } = useTranslation('article');
+    const { id } = useParams<{id: string}>();
+
+    if (!id) {
+        return (
+            <div className={classNames(cls.ArticleDetailsPage, {}, [className])}>
+                {t('Статья не найдена')}
+            </div>
+        );
+    }
 
     return (
         // eslint-disable-next-line i18next/no-literal-string
         <div className={classNames(cls.articleDetailsPage, {}, [className])}>
-            ArticleDetailsPage
+            <ArticleDetails id={id} />
         </div>
     );
 };
