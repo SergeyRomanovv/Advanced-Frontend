0a7df53432e2e18c5d06acbd2d0f35b6ae8be2a9
Sergey Romanov <sergeyromanovv94@gmail.com>
2024-05-09 22:22:36 +0300

    add skeleton, code, article details card

--- src/entities/Article/ui/ArticleCodeBlockComponent/ArticleCodeBlockComponent.tsx
+++ src/entities/Article/ui/ArticleCodeBlockComponent/ArticleCodeBlockComponent.tsx
@@ -1,19 +1,20 @@
-import { FC } from 'react';
-import { useTranslation } from 'react-i18next';
+import { memo } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
+import { Code } from 'shared/ui/Code/Code';
 import cls from './ArticleCodeBlockComponent.module.scss';
+import { ArticleCodeBlock } from '../../model/types/article';
 
 interface ArticleCodeBlockComponentProps {
    className?: string;
+   block: ArticleCodeBlock;
 }
 
-export const ArticleCodeBlockComponent: FC<ArticleCodeBlockComponentProps> = (props) => {
-    const { className } = props;
-    const { t } = useTranslation();
+export const ArticleCodeBlockComponent = memo((props: ArticleCodeBlockComponentProps) => {
+    const { className, block } = props;
 
     return (
-        <div className={classNames(cls.articleCodeBlockComponent, {}, [className])}>
-            ArticleCodeBlockComponent
+        <div className={classNames(cls.ArticleCodeBlockComponent, {}, [className])}>
+            <Code text={block.code} />
         </div>
     );
-};
+});
