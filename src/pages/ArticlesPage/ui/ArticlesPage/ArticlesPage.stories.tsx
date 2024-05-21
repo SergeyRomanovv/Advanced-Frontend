6baefa7e433a6d7e55a5dbed8e9e7dfab5f06c91
Sergey Romanov <sergeyromanovv94@gmail.com>
2024-05-21 18:17:00 +0300

    add filters, sort, search

--- src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.stories.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.stories.tsx
@@ -1,8 +1,9 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
+
 import ArticlesPage from './ArticlesPage';
 
 export default {
-    title: 'shared/ArticlesPage',
+    title: 'pages/Article/ArticlesPage',
     component: ArticlesPage,
     argTypes: {
         backgroundColor: { control: 'color' },
@@ -12,6 +13,4 @@ export default {
 const Template: ComponentStory<typeof ArticlesPage> = (args) => <ArticlesPage {...args} />;
 
 export const Normal = Template.bind({});
-Normal.args = {
-
-};
+Normal.args = {};
