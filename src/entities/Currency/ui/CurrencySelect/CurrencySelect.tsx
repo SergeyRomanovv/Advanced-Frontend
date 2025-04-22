6da2cecf88aa1c224b73211cf3ad01c09b27a142
Sergey Romanov <sergeyromanovv94@gmail.com>
2025-04-22 23:56:14 +0300

    add dropdown, update navbar, add avatar

--- src/entities/Currency/ui/CurrencySelect/CurrencySelect.tsx
+++ src/entities/Currency/ui/CurrencySelect/CurrencySelect.tsx
@@ -48,7 +48,7 @@ export const CurrencySelect = memo((props: CurrencySelectProps) => {
             items={options}
             onChange={onChangeHandler}
             readonly={readOnly}
-            direction='top'
+            direction='top right'
         />
     );
 });
