07f6eb9e222b8e01574aa8763a73570bf72fe00f
Sergey Romanov <sergeyromanovv94@gmail.com>
2025-05-08 22:50:32 +0300

    remove react-virtualized, fix modal and overlay, add ts morph script for imports

--- src/shared/ui/Avatar/Avatar.tsx
+++ src/shared/ui/Avatar/Avatar.tsx
@@ -1,6 +1,6 @@
 import { CSSProperties, FC, useMemo } from 'react';
-import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
+import { classNames } from '@/shared/lib/classNames/classNames';
 import cls from './Avatar.module.scss';
 
 interface AvatarProps {
