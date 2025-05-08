69fa884aae4fd804841cd5337c5cd0fc0b8fbdf1
Sergey Romanov <sergeyromanovv94@gmail.com>
2025-05-09 02:50:25 +0300

    add star rating, add rating entity and rating card

--- src/shared/ui/Popups/components/Dropdown/Dropdown.tsx
+++ src/shared/ui/Popups/components/Dropdown/Dropdown.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-array-index-key */
 import { Menu } from '@headlessui/react';
 import { Fragment, ReactNode } from 'react';
 import { classNames } from '@/shared/lib/classNames/classNames';
@@ -34,7 +35,7 @@ export const Dropdown = (props: DropdownProps) => {
                 {trigger}
             </Menu.Button>
             <Menu.Items className={classNames(cls.menu, {}, menuClasses)}>
-                {items.map((item) => {
+                {items.map((item, index) => {
                     const content = ({ active }: {active: boolean}) => (
                         <button
                             type='button'
@@ -48,14 +49,23 @@ export const Dropdown = (props: DropdownProps) => {
 
                     if (item.href) {
                         return (
-                            <Menu.Item as={AppLink} to={item.href} disabled={item.disabled}>
+                            <Menu.Item
+                                as={AppLink}
+                                to={item.href}
+                                disabled={item.disabled}
+                                key={`dropdown-key-${index}`}
+                            >
                                 {content}
                             </Menu.Item>
                         );
                     }
 
                     return (
-                        <Menu.Item as={Fragment} disabled={item.disabled}>
+                        <Menu.Item
+                            as={Fragment}
+                            disabled={item.disabled}
+                            key={`dropdown-key-${index}`}
+                        >
                             {content}
                         </Menu.Item>
                     );
