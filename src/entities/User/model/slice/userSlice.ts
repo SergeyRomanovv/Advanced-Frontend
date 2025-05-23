37ec89eaac66ad2f6b3b38bd8ca0cc9f9c97563a
Sergey Romanov <sergeyromanovv94@gmail.com>
2025-05-24 00:02:49 +0300

    add feature flags concept

--- src/entities/User/model/slice/userSlice.ts
+++ src/entities/User/model/slice/userSlice.ts
@@ -1,8 +1,11 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { USER_LOCALSTORAGE_KEY } from '@/shared/const/localStorage';
 import { UserSchema, User } from '../types/user';
+import { setFeatureFlags } from '@/shared/lib/features';
 
-const initialState: UserSchema = { _inited: false };
+const initialState: UserSchema = {
+    _inited: false,
+};
 
 export const userSlice = createSlice({
     name: 'user',
@@ -10,11 +13,14 @@ export const userSlice = createSlice({
     reducers: {
         setAuthData: (state, action: PayloadAction<User>) => {
             state.authData = action.payload;
+            setFeatureFlags(action.payload.features);
         },
         initAuthData: (state) => {
             const user = localStorage.getItem(USER_LOCALSTORAGE_KEY);
             if (user) {
-                state.authData = JSON.parse(user);
+                const json = JSON.parse(user) as User;
+                state.authData = json;
+                setFeatureFlags(json.features);
             }
             state._inited = true;
         },
